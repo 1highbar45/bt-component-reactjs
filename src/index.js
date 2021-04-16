de580684be068bba91f6802e791b3aebfd9277d7
1highbar45 <lhlvhsg@gmail.com>
2021-04-16 18:31:59 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 // set up Bootstrap
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-// import "../node_modules/jquery/dist/jquery.slim";
-// import "../node_modules/popper.js/dist/popper";
+import "../node_modules/jquery/dist/jquery.slim";
+import "../node_modules/popper.js/dist/popper";
 import "../node_modules/bootstrap/dist/js/bootstrap";
 
 ReactDOM.render(
